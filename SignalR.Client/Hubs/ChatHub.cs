da86b20e3ce012d65d6a2a9e49970c5fcf83d772
Jhonatan Goñe Bicharra <jhonatan.gone@hotmail.com>
2022-03-08 21:07:41 -0500

    Agregar archivos de proyecto.

---
+++ SignalR.Client/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalR.Client.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            //Enviar mensaje a todos clientes que están conectados
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
